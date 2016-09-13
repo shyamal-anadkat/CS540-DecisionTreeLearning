c621f930bf66a381fcd0980a9ca3cec265c51301
zostale <shyamalanadkat@gmail.com>
2016-09-13 10:27:30 -0500

    SA 9/13 fixed mapping issue - binary feature obj - tested with wine quality data

--- src/HW0.java
+++ src/HW0.java
@@ -1,7 +1,7 @@
 /**
  * CS540 HW0: Hand-in Practice and Parsing ML Datasets
  * 
- * @author Shyamal Anadkat (NetID: anadkat)
+ * @author Shyamal Anadkat (NetID: anadkat) 9/13/2016
  *
  */
 import java.io.File;
@@ -23,6 +23,7 @@ public class HW0 {
 	static int numExamples; //tot. examples in dataSet 
 	static int numLabels; //tot. output labels in dataSet 
 	static Map<String,List<String>> featuresToValues = new LinkedHashMap<String,List<String>>(); //preserving order
+	static List<Feature> listOfFeatureValues = new ArrayList<Feature>();
 	static List<Example> examples = new ArrayList<Example>();
 	static List<String> outputLabels = new ArrayList<String>();
 	static int l1Count=0,l2Count=0;
@@ -65,15 +66,20 @@ public class HW0 {
 				}  
 			} else if(line.contains("-")) {  //checking for features 
 
-				List<String> listOfFeatureValues = new ArrayList<String>();
+				List<String> listOfFeats = new ArrayList<String>();
 				String[] tokens = line.split("-");
 				String feature = tokens[0].trim();
 				String[] values = tokens[1].trim().split("\\s+");
 
+				Feature newFeat = new Feature(values[0].trim(), values[1].trim());
+				newFeat.setIdentifier(feature);
 				for(int i = 0; i < values.length; i++) {
-					listOfFeatureValues.add(values[i].trim());
+					listOfFeats.add(values[i].trim());
 				}
-				featuresToValues.put(feature, listOfFeatureValues);
+				newFeat.setIdentifier(feature);
+				listOfFeatureValues.add(newFeat);
+				featuresToValues.put(feature, listOfFeats);
+
 			} else if(line.trim().split(" ").length == 1) {
 				outputLabels.add(line);
 				numLabels++;
@@ -90,16 +96,16 @@ public class HW0 {
 				examples.add(dataEntry);
 			}
 		}
-		//printing relevant info after processing the parsed dataset 
 		processDataAndPrint();
 	}
 	/**
-	 * Extract count of value from a label 
+	 * Extract count of value from a label, feature
 	 * @param label
 	 * @param value
+	 * @param feature
 	 * @return
 	 */
-	public static int extractCount(String label, String value) {
+	public static int extractCount(String label, String value, String feature) {
 		int count = 0 ; 
 		for (Example ex: examples) {
 			if (ex.getLabel().equalsIgnoreCase(label)) {
@@ -107,10 +113,12 @@ public class HW0 {
 				while (it.hasNext()) {
 					Entry<String, List<String>> pair = it.next();
 					List<String> vals = pair.getValue();
-					for (String val : vals) {
-						if(val.equalsIgnoreCase(value)) {
-							count++;
-						};
+					for(int k = 0; k < vals.size(); k++) {
+						if(getFeatureIDFromIndex(k).equalsIgnoreCase(feature)) {
+							if(vals.get(k).equalsIgnoreCase(value)) {
+								count++;
+							}							
+						}						
 					}
 				}
 			}
@@ -165,12 +173,27 @@ public class HW0 {
 				for(int i = 0 ; i < numLabels; i++) {
 					int totalCount = (i==0) ? l1Count:l2Count;
 					System.out.println("Label: "+outputLabels.get(i)+" || Value: "+vals.get(v)+ " || Percent: "+ 
-							getPercent(extractCount(outputLabels.get(i),vals.get(v)),totalCount)+" "
-							+ "|| Count: "+extractCount(outputLabels.get(i),vals.get(v)));
+							getPercent(extractCount(outputLabels.get(i),vals.get(v),pair.getKey()),totalCount)+" "
+							+ "|| Count: "+extractCount(outputLabels.get(i),vals.get(v),pair.getKey()));
 				}
 			}
 			System.out.println();
 		}
+	}
+
+	/**
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public static String getFeatureIDFromIndex (int index) {
+		String retVal = null;
+		for (int i = 0 ; i < listOfFeatureValues.size(); i ++) {
+			if (i == index) {
+				retVal = listOfFeatureValues.get(i).getIdentifier();
+			}
+		}
+		return retVal;
 	}
 }
 
@@ -186,7 +209,7 @@ class Example {
 	Example(String name, String label) {
 		this.name = name;
 		this.label = label;
-		this.featureValues = new HashMap<String,List<String>>();
+		this.featureValues = new LinkedHashMap<String,List<String>>(); //preserve order
 	}
 	public void addFeatures(List<String> features) {
 		featureValues.put(label, features);
@@ -201,5 +224,28 @@ class Example {
 		return featureValues;
 	}
 }
+
+/**
+ * Represents a single feature.
+ * @author Shyamal Anadkat
+ *
+ */
+class Feature{
+	String identifier,val1,val2;
+
+	public Feature() {}
+	public Feature(String val1, String val2) {
+		this.val1 = val1;
+		this.val2 = val2;
+	}
+	public String getIdentifier() {return this.identifier;}
+	public String getVal1() {return this.val1;}
+	public String getVal2() {return this.val2;}
+	public void setVal1(String val1) {this.val1 = val1;}
+	public void setVal2(String val2) {this.val2 = val2;}
+	public void setIdentifier(String id) {this.identifier = id;}
+
+}
+
 
 
